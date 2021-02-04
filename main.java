998c3196929f6b09a1073dabbe249ccde52a5f17
televisedbrain <devkornish@gmail.com>
2021-02-04 01:49:53 +0000

    big change

--- main.java
+++ main.java
@@ -1,5 +1,32 @@
-public class Main {
+
+import java.util.*;
+
+class Input {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("yo homie what your status at num 1?");
+        int number1 = input.nextInt();
+      //  System.out.println(number1);
+
+        
+        System.out.println("yo homie what your status at num 2?");
+        int number2 = input.nextInt();
+      //  System.out.println(number2);
+        
+        
+        int total = number1 + number2;      
+        System.out.println("the total is " + total);
+        input.close();
+
+
+
+
+        
     }
+
+
 }
